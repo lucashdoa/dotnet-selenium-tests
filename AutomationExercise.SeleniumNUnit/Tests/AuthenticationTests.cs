3827e2be330be5c89801d3420c8ddd599480c7fb
lucas <lucashdoa@gmail.com>
2024-12-23 15:41:59 -0300

    Add ExtentReport

--- AutomationExercise.SeleniumNUnit/Tests/AuthenticationTests.cs
+++ AutomationExercise.SeleniumNUnit/Tests/AuthenticationTests.cs
@@ -1,8 +1,8 @@
 using AutomationExercise.SeleniumNUnit.Constants;
-using AutomationExercise.SeleniumNUnit.Drivers;
+using AutomationExercise.SeleniumNUnit.Utils;
 using AutomationExercise.SeleniumNUnit.Models.Browsers;
 using AutomationExercise.SeleniumNUnit.Pages;
-using AutomationExericse.SeleniumNUnit.Models.Users;
+using AutomationExercise.SeleniumNUnit.Models.Users;
 using OpenQA.Selenium;
 
 namespace AutomationExercise.SeleniumNUnit.Tests;
@@ -10,18 +10,25 @@ namespace AutomationExercise.SeleniumNUnit.Tests;
 public class AuthenticationTests
 {
     private IWebDriver driver;
-    private HomePage? homePage;
-    private SignupPage? signupPage;
-    private AccountCreatedPage? accountCreatedPage;
+    private ScreenshotHelper screenshotHelper;
+    private HomePage homePage;
+    private SignupPage signupPage;
+    private AccountCreatedPage accountCreatedPage;
+    private AccountDeletedPage accountDeletedPage;
 
     [SetUp]
     public void Setup()
     {
         driver = DriverFactory.CreateDriver(BrowserType.CHROME);
         driver.Manage().Window.Maximize();
+
+        ReportManager.Setup();
+        screenshotHelper = new ScreenshotHelper(driver);
+
         homePage = new HomePage(driver);
         signupPage = new SignupPage(driver);
         accountCreatedPage = new AccountCreatedPage(driver);
+        accountDeletedPage = new AccountDeletedPage(driver);
 
         homePage.NavigateTo(Routes.home);
     }
@@ -29,40 +36,61 @@ public class AuthenticationTests
     [TearDown]
     public void TearDown()
     {
+        ReportManager.Flush();
         driver.Dispose();
     }
 
-    [Test]
+    [Test, Category("TC01"), Category("Smoke")]
     public void RegisterUser()
     {
-        UserAddress address = new(
-            "John",
-            "Doe",
-            "Test Street, Number 7 Apartment 101",
-            "United States",
-            "Las Vegas",
-            "Nevada",
-            "123456789",
-            "98765432"
-        );
+        try
+        {
+            ReportManager.StartTest("Test Case 1: Register User Test", "Testing account creation functionality");
 
-        User user = new(
-            "validuser",
-            "validuser12@email.com",
-            true,
-            "secretpassword",
-            "25",
-            "10",
-            "1984",
-            true,
-            true,
-            address
-        );
+            UserAddress address = new(
+                "John",
+                "Doe",
+                "Test Street, Number 7 Apartment 101",
+                "United States",
+                "Las Vegas",
+                "Nevada",
+                "123456789",
+                "98765432"
+            );
 
-        signupPage.SignupUserWithEmail("testemail", "testemail@mail.com");
-        signupPage.EnterAccountInformation(user);
+            User user = new(
+                "validseleniumuser",
+                "validseleniumuser@email.com",
+                true,
+                "secretpassword",
+                "25",
+                "10",
+                "1984",
+                true,
+                true,
+                address
+            );
 
-        accountCreatedPage.Continue();
-        homePage.DeleteAccount();
+            signupPage.SignupUserWithEmail(user.name, user.email);
+            signupPage.EnterAccountInformation(user);
+            ReportManager.Log("Created account successfully");
+
+            Assert.AreEqual("ACCOUNT CREATED!", accountCreatedPage.GetAccountCreatedText());
+            ReportManager.AttachScreenshot(screenshotHelper.CaptureScreenshot("account-created-successfully"));
+
+            accountCreatedPage.Continue();
+            homePage.DeleteAccount();
+
+            Assert.AreEqual("ACCOUNT DELETEDA!", accountDeletedPage.GetAccountDeletedText());
+            ReportManager.AttachScreenshot(screenshotHelper.CaptureScreenshot("account-deleted-successfully"));
+            ReportManager.Log("Deleted account successfully");
+        }
+        catch (Exception e)
+        {
+            string errorScreenshotPath = screenshotHelper.CaptureScreenshot("TC01_Failed");
+            ReportManager.AttachScreenshot(errorScreenshotPath);
+            ReportManager.FailTest(e.Message);
+            throw;
+        }
     }
 }
---
+++ AutomationExercise.SeleniumNUnit/Utils/ReportManager.cs
@@ -0,0 +1,44 @@
+using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+
+namespace AutomationExercise.SeleniumNUnit.Utils;
+
+public class ReportManager
+{
+    private static ExtentReports _extent;
+    private static ExtentTest _test;
+    private static ExtentSparkReporter _htmlReporter;
+
+    public static void Setup()
+    {
+        string reportPath = Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "Reports", "ExtentReport.html");
+        _htmlReporter = new ExtentSparkReporter(reportPath);
+        _extent = new ExtentReports();
+        _extent.AttachReporter(_htmlReporter);
+    }
+
+    public static void StartTest(string testName, string testDescription)
+    {
+        _test = _extent.CreateTest(testName, testDescription);
+    }
+
+    public static void FailTest(string errorMessage)
+    {
+        _test.Fail(errorMessage);
+    }
+
+    public static void AttachScreenshot(string screenshotPath)
+    {
+        _test.AddScreenCaptureFromPath(screenshotPath);
+    }
+
+    public static void Log(string message)
+    {
+        _test.Log(Status.Info, message);
+    }
+
+    public static void Flush()
+    {
+        _extent.Flush();
+    }
+}
